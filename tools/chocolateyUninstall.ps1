b32feee7266cc09eb9d9813b3b51e873dda1c240
Darwin JS <darwinjsgithub.services@sanoys.com>
2015-06-22 07:30:47 -0400

    fixups

--- tools/chocolateyUninstall.ps1
+++ tools/chocolateyUninstall.ps1
@@ -3,11 +3,10 @@ $installerType = 'MSI'
 $url = 'http://download.microsoft.com/download/9/1/E/91E9F42C-3F1F-4AD9-92B7-8DD65DA3B0C2/mvmc_setup.msi'
 $silentArgs = '/qn /l*v $env:temp\uninstallvirtualmachineconverter.log'
 $validExitCodes = @(0,3010)
-$InstallerLogLocation = "$env:temp\uninstallvirtualmachineconverter.log"
 
 Try 
   {
-  msiexec.exe /x "{332C1E78-1D2F-4A64-B718-68095DC6254B}" "$silentArgs"
+  Start-Process "msiexec.exe"  -ArgumentList "/x {332C1E78-1D2F-4A64-B718-68095DC6254B} $silentArgs" -Wait
   }
 
 Catch 
